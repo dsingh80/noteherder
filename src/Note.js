4b4cd773083202703210c6eb7952f18bc09c05b5
="Damanveer Singh <=>
2017-06-22 09:57:34 -0400

    Minor update

--- src/Note.js
+++ src/Note.js
@@ -2,9 +2,8 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 
 const Note = ({ note, setCurrentNote, removeNote }) => {
-
+  
   return (
-
       <NavLink to={`/notes/${note.id}`}>
         <li>
           <div className="note">
